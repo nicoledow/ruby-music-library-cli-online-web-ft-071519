30911a81c7558832eed60d4e574f3a2b4c9507dc
Nicole Dow <nikki.dow71@gmail.com>
2019-08-02 21:51:11 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -12,9 +12,7 @@ class MusicImporter
   end
   
   def import
-    #iterate through files
-    #call .create_from_filename on each filename
-    
+    self.files.each {|file| Song.create_from_filename(file)}
   end
   
   
