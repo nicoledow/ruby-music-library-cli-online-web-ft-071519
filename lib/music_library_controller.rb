9f7daa1ae4309ee5d194f9013a7a31efb8e103ab
Nicole Dow <nikki.dow71@gmail.com>
2019-08-03 14:30:00 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -10,6 +10,7 @@ class MusicLibraryController
   end
   
   def call
+    while user_input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -20,10 +21,21 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     
-   user_input = gets.strip
+   user_input = gets.strip.downcase
    
+    case user_input
+    when "list songs"
+      list_songs
+    when "list artists"
+      list_artists
+    when "list genres"
+      list_genres
+    when "list artist"
+      list_songs_by_artist
+    when "play song"
+      play_song
+    end
   
-    
   end
   
   def list_songs
@@ -50,7 +62,37 @@ class MusicLibraryController
     end
   end
   
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    
+    songs_by_artist = Song.all.select {|s| s.artist.name.downcase == input.downcase}
+    songs_by_artist.sort_by! {|song| song.name}
+    songs_by_artist.each_with_index do |song, i|
+      puts "#{i + 1}. #{song.name} - #{song.genre.name}"
+    end
+  end
   
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    
+    songs_in_genre = Song.all.select {|s| s.genre.name.downcase == input.downcase}
+    songs_in_genre.sort_by! {|song| song.name}
+    songs_in_genre.each_with_index do |song, i|
+      puts "#{i + 1}. #{song.artist.name} - #{song.name}"
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+    alphabetized_by_title = Song.all.sort_by {|song| song.name}
+    
+    if input.between?(1, (Song.all.length - 1))
+      puts "Playing #{alphabetized_by_title[input - 1].name} by #{alphabetized_by_title[input - 1].artist.name}"
+    end
+  end
   
   
 end
