f66d05a05e759753cebee0e02a656dbdd5ceaac2
Nicole Dow <nikki.dow71@gmail.com>
2019-08-03 14:37:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -33,6 +33,8 @@ class MusicLibraryController
       list_artists
     when "list genre"
       list_songs_by_genre
+    when "list genres"
+      list_genres
     when "list artist"
       list_songs_by_artist
     when "play song"
